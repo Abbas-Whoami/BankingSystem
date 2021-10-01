cb1e3d5f54fd9290cfc151edaf47aa94ccc1b537
Mohamed Abbas <44034794+Abbas-Whoami@users.noreply.github.com>
2021-10-01 12:32:29 +0530

    Update mongoose.js

--- models/mongoose.js
+++ models/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://abbas:Abbas@22@cluster0.typoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", { 'useNewUrlParser': true, 'useFindAndModify': false, 'useUnifiedTopology': true, 'useCreateIndex': true }, (err) => {
+mongoose.connect("<mongoDB>", { 'useNewUrlParser': true, 'useFindAndModify': false, 'useUnifiedTopology': true, 'useCreateIndex': true }, (err) => {
     if (err) return console.log(err)
     console.log('[mongoose service ... OK]')
 })
